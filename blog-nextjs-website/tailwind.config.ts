61c0d858948744a6223fa366835c0812788f9440
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-10-13 16:33:02 +0530

    tailwind css added successfully

--- blog-nextjs-website/tailwind.config.ts
+++ blog-nextjs-website/tailwind.config.ts
@@ -8,12 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary:'#53B095',
+        secondary:'#2c785c'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontFamily: {
+      sans:['Roboto', 'sans-sarif']
+    }
   },
   plugins: [],
 }
